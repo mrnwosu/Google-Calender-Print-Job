99832a01951c5cbf140996ecaf08837ce36a67f8
Ike Nwosu <ike.nwosu.151@gmail.com>
2022-10-27 17:57:03 -0400

    IE Print Update

--- test.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module  "./Itinerary Printer.ps1";
-
-print-google-calendar-itinerary -targetEmail "ike.nwosu.151@gmail.com" `
-    -certPath ".\get-calenday-events-8bca31a28b28.p12" `
-    -certPassword "notasecret" `
-    -serviceAccount "ps-cal-list@get-calenday-events.iam.gserviceaccount.com" `
-    -printerName "$ Test Printer"
